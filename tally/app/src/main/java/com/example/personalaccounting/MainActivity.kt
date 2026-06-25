4a9838df2efb5f57dd7b704fd2bf813144c2140b
“fanzhendong” <zhendong.fan@yuantiaokj.com>
2026-06-25 09:10:09 +0800

    feat: set up basic Android project with Compose

---
+++ tally/app/src/main/java/com/example/personalaccounting/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.personalaccounting
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.personalaccounting.ui.theme.PersonalAccountingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PersonalAccountingTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PersonalAccountingTheme {
+        Greeting("Android")
+    }
+}
